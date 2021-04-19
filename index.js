34acdbbddc71b9b24bae15b0052bfe36af274497
nickfurious <datfuriouskid@live.com>
2021-04-19 15:20:26 -0700

    1 file modified

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ function removeObj(obj, key) {
 }
 
 console.log(removeObj(myObj, 'a'));
+
+function deleteProperty(myObj, prop) {
+  let newObj = {};
+  for(let key in myObj) {
+    if(key !== prop) {
+      newObj[key] = myObj[key];
+    }
+  }
+  return newObj;
+}
+
+console.log(deleteProperty({a: 1, b: 2,}, 'a'));
