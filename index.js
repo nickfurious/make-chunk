519d665ba0163a20623b63eda2106af26ba23d8a
nickfurious <datfuriouskid@live.com>
2021-04-19 15:08:30 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-let arr = [33, 21, 14, 3, 21, 21, 334, 579, 34, 565, 4345, 3987, 3];
+let myObj = {
+  a: 1,
+  b: 2,
+  c: 3
+};
 
-for(let i = 0; i < arr.length; i++) {
-  for(let j = i + 1; j < arr.length; j++) {
-    if (arr[i] == arr[j]) {
-      arr.splice(j, 1);
-      j--;
-    }
-  }
+console.log(myObj);
+
+function removeObj(obj, key) {
+  delete obj[key];
+  return obj;
 }
-console.log(arr)
 
-// Working with splice()
-// var fruits = ["Banana", "Orange", "Apple", "Mango"];
-// document.getElementById("demo").innerHTML = fruits;
-
-// function myFunction() {
-//   fruits.splice(2, 0, "Lemon", "Kiwi");
-//   document.getElementById("demo").innerHTML = fruits;
-// }
+console.log(removeObj(myObj, 'a'));
